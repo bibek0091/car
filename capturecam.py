f52da1f0d559091fb11ea521869613f8f1356e41
bibek0091 <23mi008@kpriet.ac.in>
2026-02-24 23:04:03 +0530

    test4

--- capturecam.py
+++ capturecam.py
@@ -147,8 +147,9 @@ class ManualSteeringGUI:
                 # Capture directly in RGB format as configured
                 frame = self.picam2.capture_array()
                 if frame is not None:
-                    # Picamera2 often defaults to BGR arrays depending on OS bindings
-                    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    # If it's bluish, the Red and Blue channels are swapped. 
+                    # We will force a physical numpy slice swap to guarantee the flip.
+                    frame_rgb = frame[:, :, ::-1]
                     self.latest_frame = frame_rgb
                     img = Image.fromarray(frame_rgb)
                     imgtk = ImageTk.PhotoImage(image=img)
