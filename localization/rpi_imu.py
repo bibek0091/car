37457b0d5d3b156daca9eefc05acd0023562200b
bibek0091 <23mi008@kpriet.ac.in>
2026-02-26 02:08:30 +0530

    imu

--- localization/rpi_imu.py
+++ localization/rpi_imu.py
@@ -10,8 +10,33 @@ def main():
     print("Attempting to connect to BNO055 via Pi Hardware I2C...\n")
 
     try:
-        # Initialize I2C bus on Raspberry Pi Pins 3 (SDA) and 5 (SCL)
-        i2c = busio.I2C(board.SCL, board.SDA)
+        # Initialize Software I2C bus (bus 3) to bypass Pi hardware bugs
+        import smbus2
+        
+        try:
+            # We use smbus2 to open bus 3, but Adafruit library expects a busio.I2C object. 
+            # We can force board.I2C() or use an alternative library, but standard busio doesn't easily let us pick bus 3.
+            # INSTEAD: We will just tell the adafruit library to use bus 3 by mocking the I2C interface.
+            pass
+        except Exception:
+            pass
+
+        # The easiest way to select an arbitrary I2C bus via Adfruit Blinka is:
+        import busio
+        import microcontroller
+        
+        # In Blinka, to force bus 3 (which we create via config.txt overlay), we use the extended board pins if they exist,
+        # OR just use the specific smbus/adafruit_extended_bus library. Since this is a simple script, we'll
+        # just use `board.I2C()` and assume the default maps to 1. 
+        # Actually, Blinka allows configuring the default I2C bus number via an environment variable, 
+        # but let's just use `adafruit_extended_bus` if available, otherwise fallback to standard.
+        try:
+            from adafruit_extended_bus import ExtendedI2C as I2C
+            i2c = I2C(3)
+        except ImportError:
+            print("Installing adafruit-extended-bus is recommended.")
+            print("Falling back to default bus.")
+            i2c = busio.I2C(board.SCL, board.SDA)
         
         # Try default Adafruit address (0x28) first, then alternative (0x29)
         sensor = None
